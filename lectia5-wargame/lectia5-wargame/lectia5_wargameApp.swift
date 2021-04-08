87d98cb8f30c12a898285e26c925d000ba9b532d
marianul-commits <80788036+marianul-commits@users.noreply.github.com>
2021-04-08 20:20:46 +0300

    Add files via upload

---
+++ lectia5-wargame/lectia5-wargame/lectia5_wargameApp.swift
@@ -0,0 +1,17 @@
+//
+//  lectia5_wargameApp.swift
+//  lectia5-wargame
+//
+//  Created by Marian Nasturica on 12.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct lectia5_wargameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
